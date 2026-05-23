0b6d9fd8b9be524a9e851d84f73ecc80d6259b8e
9h9pksfxzf-byte <9h9pksfxzf@privaterelay.appleid.com>
2026-05-23 13:33:52 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen text-white text-4xl">
+      <h1>HybridStretch App läuft!</h1>
+    </div>
+  )
+}
